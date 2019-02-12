0f4924e93f8fe973d4e3b328ac2dcbf264f0cf4a
Jamie Dawson <jadawson@e1z3r4p21.42.us.org>
2019-02-11 19:37:33 -0800

    EqualSumChecker folder

---
+++ EqualSumChecker/EqualSumChecker.java
@@ -0,0 +1,14 @@
+public class EqualSumChecker {
+     public static boolean hasEqualSum(int one, int two, int three){
+        if (one + two == three)
+            return (true);
+        else
+            return (false);
+    }
+
+}
+
+/*
+ if the first and second int equal the third int, return true. 
+ Else, return false.
+ */
