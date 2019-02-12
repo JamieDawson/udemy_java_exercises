8ba7e28a7f2128c11ede940671055d3a87dbcdba
Jamie Dawson <jadawson@e1z3r4p21.42.us.org>
2019-02-11 19:40:50 -0800

    checks if numbers are in between 13 and 19

---
+++ section_4/TeenNumberChecker/TeenNumberChecker.java
@@ -0,0 +1,29 @@
+public class TeenNumberChecker {
+    public static void  main(String[] args){
+        int one = 20;
+        if (isTeen(one) == true)
+            System.out.println("this is true");
+        else
+            System.out.println("false as hell");
+    }
+
+    public static boolean hasTeen(int one, int two, int three)
+    {
+        if (one >= 13 && one <= 19)
+            return (true);
+        else if (two >= 13 && two <= 19)
+            return (true);
+        else if (three >= 13 && three <= 19)
+            return (true);
+        else
+            return (false);
+    }
+    public static boolean isTeen(int one)
+    {
+        if (one >= 13 && one <= 19)
+            return (true);
+        else
+            return (false);
+    }
+}
+
